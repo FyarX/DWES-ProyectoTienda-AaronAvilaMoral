8c5e330926839e327d2b8cdab5b13fdd98426c13
FyarX <aaronavilamoralrob@gmail.com>
2025-02-10 19:48:41 +0100

    adicion namespaces modelos/controladores

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ require __DIR__ ."./vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ );
 $dotenv->load();
 
-define("DBHOST", $_ENV['DBHOST']);
-define("DBUSER", $_ENV['DBUSER']);
-define("DBPASS", $_ENV['DBPASS']);
-define("DBNAME", $_ENV['DBNAME']);
+define("DB_HOST", $_ENV['DB_HOST']);
+define("DB_USER", $_ENV['DB_USER']);
+define("DB_PASS", $_ENV['DB_PASS']);
+define("DB_NAME", $_ENV['DB_NAME']);
