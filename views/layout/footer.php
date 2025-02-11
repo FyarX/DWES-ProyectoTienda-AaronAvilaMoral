9eadff157c7706e3918ca6fa1354701f318e3dce
FyarX <aaronavilamoralrob@gmail.com>
2025-02-11 01:03:12 +0100

    implementación header + footer

---
+++ views/layout/footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
+    <title>Footer</title>
+</head>
+<body>
+
+<footer class="p-4 bg-white md:p-8 lg:p-10">
+  <div class="mx-auto max-w-screen-xl text-center">
+      <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900">
+          Proyecto Tienda &#x1F3EA  
+      </a>
+      <p class="my-6 text-gray-500">Esto es el proyecto final de la asignatura de DWES</p>
+      <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900">
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Premium</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">Campaigns</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Blog</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Affiliate Program</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">FAQs</a>
+          </li>
+          <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">Contact</a>
+          </li>
+      </ul>
+      <span class="text-sm text-gray-500 sm:text-center">© 2025 <a href="#" class="hover:underline">Aarón Ávila Moral</a>. Todos los derechos reservados.</span>
+  </div>
+</footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+</body>
+</html>
